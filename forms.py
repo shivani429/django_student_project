6fbc6e641943d1a1d44356c12167027a376f2190
shivani429 <113819244+shivani429@users.noreply.github.com>
2022-11-05 16:28:52 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,14 +1,6 @@
 from dataclasses import fields
 from django import forms
 from demoapp.models import *
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-import datetime
-
-class LoginForm(forms.Form):
-    name = forms.CharField(label='Username', max_length=100)
-    email = forms.EmailField(label='Email', max_length=100)
-    password = forms.CharField(label='Password')
 
 class StudentForm(forms.Form):
     name = forms.CharField(label='name', max_length=50)
@@ -16,32 +8,3 @@ class StudentForm(forms.Form):
     class_no = forms.IntegerField(label='class no')
     address = forms.CharField(label='Address', max_length=70)
  
-class EmployeeForm(forms.ModelForm):  
-    class Meta:  
-        model = Employees 
-        fields = "__all__"  
-
-class CreateUserForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username','email','password1','password2']
-
-class PhotoForm(forms.ModelForm):
-    class Meta:
-        model = Photo
-        fields = ['name', 'description']
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = '__all__' 
-
-class EmpForm(forms.ModelForm):
-    class Meta:
-        model = Emp
-        fields = '__all__'
-        
-class QuizModelForm(forms.ModelForm):
-    class Meta:
-        model = QuizModel
-        fields = '__all__'
